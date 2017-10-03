965dafcb78e729c8fd106db6dcbbb80768d3ee10
Maxim Grib <max.grib@gmail.com>
2017-10-03 16:38:17 +0300

    update mock server

--- mock.js
+++ mock.js
@@ -1,7 +1,35 @@
-var nock = require('nock');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const http = require('http');
+const delay = require('./middlewares/delay');
+const logger = require('./middlewares/logger');
+const log = require('./utils/log');
 
-console.log('Mock server is running..');
+module.exports = (options = {}) => {
+  if (!options.routes) throw new Error('[ ag-mock-server ] Routes should be provided');
 
-var scope = nock(/example\.com/)
-      .get('/resource')
-      .reply(200, 'domain regex matched');
+  const appRoutes = require(options.routes);
+  const app = express();
+
+  app.use(cors());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+  app.all('*', logger);
+  app.all('*', delay);
+
+  // register additional routes
+  if (options.routes) {
+    appRoutes(app, options);
+  }
+
+  const port = options.port || 1234;
+
+  // init server
+  http.createServer(app).listen(port, () => {
+    log.info('[ ag-mock-server ] Development server is established on %s.', port);
+  });
+};
