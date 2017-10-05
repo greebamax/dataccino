f8c291abf76620c8460c4f430568412bb951c67e
Maxim Grib <max.grib@gmail.com>
2017-10-05 15:31:50 +0300

    allow to customize delay of response

--- middlewares/delay.js
+++ middlewares/delay.js
@@ -1,9 +1,15 @@
 /**
  * Delay middleware
- * Used to simulate up to 1 sec delay on each response.
+ * Used to simulate delay on each response up to 1 sec (by default).
+ * Can be customized by URL query string parameter `delay` (in ms).
  */
 module.exports = (req, res, next) => {
-  const delay = Math.floor((Math.random() * 10)) * 100;
+  let delay = parseInt(req.query.delay);
+
+  if (!delay) {
+    delay = Math.floor((Math.random() * 10)) * 100;
+  }
+
   setTimeout(() => {
     next();
   }, delay);
